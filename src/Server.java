e3e2ea020451a770afb132e8d5d832ac666ef1bb
babalolajnr <babalolajnr@gmail.com>
2021-04-08 14:14:39 +0100

    added comment

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,6 @@
 public class Server {
     public static void main(String[] args) {
+        // run the derver on port 4000
         ServerSystem server = new ServerSystem(4000);
         server.runSystem();
     }
